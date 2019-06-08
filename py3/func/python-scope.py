662ab7f3d30bf7022371b2a87c763a9246694fec
returntears <returntears@outlook.com>
2019-06-08 09:53:19 +0800

    python: 作用域scope

--- py3/func/python-scope.py
+++ py3/func/python-scope.py
@@ -21,12 +21,24 @@ def outer():
 # 变量的修改
 count = 10
 def outer2():
-    # print函数的
-    print(count)
+    # 局部作用域中声明某个变量是全局的
+    global count
+    print(count)    # 10
     # count = 5 这里会报错,但是去掉print函数就正确。因为局部不能修改全部变量，去掉print表示重新定义局部变量
     # count += 1 这里没有上面的两行语句依然报错，因为count = count + 1, 第一个count表示局部变量，第二个count表示全局变量，局部不能修改全部变量
+    # 想要修改的话，需要用到global关键字声明一下
+    count += 1
+    print(count)    # 11
+outer2()
 
-
+'''
+小结:
+    1、变量的查找顺序: lEGB
+    2、只有模块、类、以及函数才能引入新作用域
+    3、对于一个变量，内部作用域先声明就会覆盖外部变量，不声明直接使用就会使用外部作用域的变量
+    4、内部作用域要修改外部作用域变量的值时，全局变量要使用global关键字，嵌套作用域变量要使用nonlocal关键字，(nonlocal是python3新增，有了它可以完美实现闭包)
+    
+'''
 
 
 
