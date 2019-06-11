04344894567b065578d613c147f339eb993ae668
returntears <returntears@outlook.com>
2019-06-11 22:46:45 +0800

    python: 高阶函数

--- py3/func/python-funcb.py
+++ py3/func/python-funcb.py
@@ -15,3 +15,27 @@ def foo():
 # 函数是默认返回None的
 a = foo()
 print(a)
+
+# 高阶函数
+# 函数本身也是一个变量, 函数名既可以作为函数参数，还可以作为函数的返回值
+def fn(n):
+    return n * n
+
+print(fn(4))
+
+def f(a, b, f):
+    return f(a) + f(b)
+
+res = f(1, 2, fn)
+print(res)
+
+def funct():
+    def inner():
+        return 7
+    return inner
+# ret是函数的inner的内存引用地址
+ret = funct()
+# print(ret)
+print(ret())
+
+
