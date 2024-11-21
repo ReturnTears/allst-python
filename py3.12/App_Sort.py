dd8d62dbb0ded17966a8fcc6af103c223cabcdeb
numb <1113239375@qq.com>
2024-11-21 13:50:46 +0800

    Add files via upload
    Sort

---
+++ py3.12/App_Sort.py
@@ -0,0 +1,6 @@
+import Bubble_Sort as bs
+numbers = [23, 32, 1, 3, 4, 19, 20, 2, 4]
+res = bs.bubble_sort(numbers)
+print(res)
+
+
---
+++ py3.12/Person.py
@@ -0,0 +1,10 @@
+class Person:
+    """人
+    :param name: 姓名
+    :param age: 年龄
+    :param favorite_color: 最喜欢的颜色
+    """
+    def __init__(self, name, age, favorite_color):
+        self.name = name
+        self.age = age
+        self.favorite_color = favorite_color
